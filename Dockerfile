e0f4f078c70ae0380abf504ca85d4c5494b67ff3
Alexandre Tellier <alexandre@MacBook-Pro-de-Alexandre.local>
2022-01-25 23:44:51 +0100

    first try for download

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:latest
 RUN apt-get update && apt-get install -y libcups2-dev
-RUN pip install pycups flask
+RUN pip install pycups flask requests
 COPY ./app ./
 CMD [ "python", "./main.py" ]
--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, Response
+import requests
 import logging
 import os
 import sys
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
 
 @app.route("/event",  methods=['GET', 'POST'])
 def event():
     
-
     if request.json is not None: 
         content = request.json
 
@@ -20,8 +22,11 @@ def event():
         if "event" in content:
             if "files" in content['event']:
                 files = content['event']['files'][0] 
-                logging.basicConfig(level=logging.DEBUG)
-                app.logger.info(files['title'])
+                
+                app.logger.info("New file sent " + str(files['title']))
+                if "url_private_download" in files:
+                    r = requests.get(files["url_private_download"], allow_redirects=True)
+                    open(files['title'], 'wb').write(r.content)
                 return Response(status=200)
 
     return Response(status=201)
